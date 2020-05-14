097d3d20b69be6738a9b14eb74b1026ec0d83197
Liibro <liina@22systems.ee>
2020-05-15 01:00:33 +0300

    Css superbasic layout

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raamaturiiul</title>
+
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.js"
+    integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+    crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+<div class="container">
+
+    <div class="item i1">
+        <div class="item__header">
+        </div><!--.item__header-->
+        <div class="item__header white">
+        </div><!--.item__header-->
+        <div class="clearfix-50"></div>
+    </div><!--.i1-->
+
+
+    <div class="item i2">
+        <div class="item__header">
+
+        </div><!--.item__header-->
+        <div class="item__header white">
+        </div><!--.item__header-->
+        <div class="clearfix-50"></div>
+
+        <div class="i2__section">
+            <div class="i2__row">
+                <div class="col__1">PEALKIRI</div>
+                <div class="col__2">AUTOR</div>
+                <div class="col__3">LEHEKÜLGI</div>
+                <div class="col__4">AASTA</div>
+                <div class="col__5">HIND</div>
+                <div class="col__6">LISATUD</div>
+                <div class="col__7">PROGRESS</div>
+                <div class="col__8">dot</div>
+            </div>
+        </div>
+
+    </div><!--.i2-->
+
+    <div class="item i3">
+        <div class="item__header">
+        </div><!--.item__headerr-->
+        <div class="item__header white">
+        </div><!--.item__header-->
+        <div class="clearfix-50"></div>
+
+        <div class="i3__section">
+            
+            <div class="i3__row">
+                <div class="col__1">PEALKIRI</div>
+                <div class="col__2">VIIMATI LOETUD</div>
+                <div class="col__3">PROGRESS</div>
+                <div class="col__8">dot</div>
+            </div>
+
+        </div>
+
+    </div><!--.i3-->
+
+    <div class="item i4">
+        <div class="item__header">
+        </div><!--.item__header-->
+        <div class="item__header white">
+        </div><!--.item__header-->
+        <div class="clearfix-50"></div>
+    </div><!--.i4-->
+
+
+</div><!--.container-->
+
+
+
+<script src="script.js"></script>
+</body>
+</html>
