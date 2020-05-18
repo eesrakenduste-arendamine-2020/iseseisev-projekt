b3746c00b00b6d9d3f48abdd8c718cb53a29ce46
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-05-18 19:28:06 +0300

    Added book adding and deleting

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
 // Raamatu klass
 class Book {
-    constructor(title, author)
+  constructor(title, author, page) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+  }
 }
 // UI klass
+class UI {
+  static displayBooks() {
+    const books = [];
+
+    books.forEach((book) => UI.addBook(book));
+  }
+
+  static addBook(book) {
+    const bookList = document.querySelector("#book-list");
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.page}</td>
+        <td><i class="fas fa-trash delete"></i></td>
+    `;
+
+    bookList.appendChild(row);
+  }
+
+  static deleteBook(target) {
+    if (target.classList.contains("delete")) {
+      target.parentElement.parentElement.remove();
+    }
+  }
+
+  static clear() {
+    document.querySelector("#book-title").value = "";
+    document.querySelector("#book-author").value = "";
+    document.querySelector("#book-page").value = "";
+  }
+}
 
 // LocalStorage klass
 
+// Näita raamatuid
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
-// Event: Display books
+// Lisa raamat
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#book-title").value;
+  const author = document.querySelector("#book-author").value;
+  const page = document.querySelector("#book-page").value;
 
-// Event: Add book
+  const book = new Book(title, author, page);
 
-// Event: Mark book as read
+  UI.addBook(book);
+
+  UI.clear();
+});
+
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  console.log(e.target);
+  UI.deleteBook(e.target);
+});
+// Märgi loetuks
