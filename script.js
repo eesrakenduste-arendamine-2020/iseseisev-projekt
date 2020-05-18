6675e032bbc3328850c37dba28985d1a87d48e4c
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-05-18 20:20:03 +0300

    Added missing code

--- script.js
+++ script.js
@@ -1,41 +1,3 @@
-/*const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark'); //add this
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light'); //add this
-    }
-}
-
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
-}*/
-
-
-
-
-
 // Raamatu klass
 class Book {
   constructor(title, author, page) {
@@ -115,6 +77,11 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
 
     UI.clear();
   }
+});
+
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  console.log(e.target);
+  UI.deleteBook(e.target);
 });
 
 // Event: Mark book as read
