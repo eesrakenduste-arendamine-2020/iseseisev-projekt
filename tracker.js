03aaa57808e0e3417f29f1571ffa01e637473b02
Anete Vaalu <anetevlu@tlu.ee>
2020-05-19 13:48:31 +0300

    navigation bar

---
+++ tracker.js
@@ -0,0 +1,30 @@
+$('.menu-item').click(function(){
+    $('.menu-item').removeClass('active');
+    $(this).addClass('active');
+    $('.menu').toggleClass('menu-active');
+    selectContent();
+});
+
+function selectContent(){
+    if($('[data-menu-search]').hasClass('active')){
+        $('#movies').hide();
+        $('#tv-shows').hide();
+        $('#my-list').hide();
+        $('#search').show();
+    } else if($('[data-menu-my-list]').hasClass('active')){
+        $('#my-list').show();
+        $('#movies').hide();
+        $('#tv-shows').hide();
+        $('#search').hide();
+    } else if($('[data-menu-movies]').hasClass('active')){
+        $('#my-list').hide();
+        $('#movies').show();
+        $('#tv-shows').hide();
+        $('#search').hide();
+    } else if($('[data-menu-tv-shows]').hasClass('active')){
+        $('#my-list').hide();
+        $('#movies').hide();
+        $('#tv-shows').show();
+        $('#search').hide();
+    }
+}
