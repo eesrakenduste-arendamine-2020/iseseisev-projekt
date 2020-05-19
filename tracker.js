9d5e136754ab92465410a9a258ecfa13fed8d09c
Anete Vaalu <anetevlu@tlu.ee>
2020-05-19 21:30:57 +0300

    api algus

--- tracker.js
+++ tracker.js
@@ -3,8 +3,47 @@ window.onload = function(){
     selectContent();
 };
 
-var popular_movies;
+import{APIKEY} from "../lamp.js"; 
+let baseURL = 'https://api.themoviedb.org/3/';
+let configData = null;
+let baseImageURL = null;
+let getConfig = function () {
+    let url = "".concat(baseURL, 'configuration?api_key=', APIKEY); 
+    fetch(url)
+    .then((result)=>{
+        return result.json();
+    })
+    .then((data)=>{
+        baseImageURL = data.images.secure_base_url;
+        configData = data.images;
+        console.log('config:', data);
+        console.log('config fetched');
+        //runSearch('jaws')
+    })
+    .catch(function(err){
+        alert(err);
+    });
+}
 
+let getPopularMovies = function(){
+    let url = "".concat(baseURL, 'movie/popular?api_key=', APIKEY, '&language=en-US');
+    fetch(url)
+    .then((result)=>{
+        return result.json();
+    })
+    .then((data)=>{
+        console.log(data);
+        console.log('popular movie data fetched');
+    })
+    .catch(function(err){
+        alert(err);
+    })
+}
+
+
+//var popular_movies;
+getPopularMovies();
+getConfig();
 $('.menu-item').click(function(){
     $('.menu-item').removeClass('active');
     $(this).addClass('active');
@@ -18,7 +57,7 @@ function selectContent(){
         $('#tv-shows').hide();
         $('#my-list').hide();
         $('#search').show();
-    } else if($('[data-menu-my-list]').hasClass('active')){
+    } else if($('[data-menu-my-list]').hasClass('active')){ 
         $('#my-list').show();
         $('#movies').hide();
         $('#tv-shows').hide();
@@ -35,5 +74,3 @@ function selectContent(){
         $('#search').hide();
     }
 }
-
-import{APIKEY} from "../lamp.js";
