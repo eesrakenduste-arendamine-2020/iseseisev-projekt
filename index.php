f26f41008993f0175d045a1bdfea16899d559a39
Liibro <liina@22systems.ee>
2020-05-20 22:22:46 +0300

    modal form jquery validation

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     src="https://code.jquery.com/jquery-3.4.1.js"
     integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
     crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js" integrity="sha256-sPB0F50YUDK0otDnsfNHawYmA5M0pjjUf4TvRJkGFrI=" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="main.css">
@@ -146,29 +147,33 @@
             <div class="dialogBox__fields">
                 <form name="book_form" action="" method="POST" class="dialogBox__form">
 
-
+                    <div class="error-title"></div>
                     <div class="inputBox">
                         <input name="title" placeholder="Pealkiri" class="inputBox__input" type="text">
                     </div>
 
+                    <div class="error-author"></div>
                     <div class="inputBox">
                         <input name="author" placeholder="Autor" class="inputBox__input" type="text">
                     </div>
 
+                    <div class="error-year"></div>
                     <div class="inputBox">
                         <input name="year" placeholder="Aasta" class="inputBox__input" type="text">
                     </div>
 
-
+                    <div class="error-pages_total"></div>
                     <div class="inputBox">
                         <input name="pages_total" placeholder="Lehekülgede arv kokku" class="inputBox__input" type="text">
                     </div>
 
+                    <div class="error-pages_finished"></div>
                     <div class="inputBox">
                         <input name="pages_finished" placeholder="Lehekülgede arv loetud" class="inputBox__input" type="text">
                     </div>
 
 
+                    <div class="error-rating"></div>
                     <div class="inputBox">
                         <input name="rating" placeholder="Hinnang (1-10)" class="inputBox__input" type="text">
                     </div>
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
     // Modal js
     $(document).on('click', '.js-add-new', function() {
         $('.addNewBookModal').css('display', 'flex');
@@ -10,6 +9,63 @@ $(document).ready(function() {
         $('.addNewBookModal').hide();
     });
 
+    $.validator.addMethod("comparison", function (done, element) {
+        var total = parseFloat($('[name="pages_total"]').val());
+        return this.optional(element) || parseFloat(done) <= total;
+    });
+
+    // Jquery validation
+    $('[name="book_form"]').validate({
+        rules: {
+            title: {
+                required: true,
+            },
+            author: {
+                required: true
+            },
+            year: {
+                 number: true
+            },
+            pages_total: {
+                required: true,
+                number: true
+            },
+            pages_finished: {
+                number: true,
+                comparison: true
+            },
+            rating: {
+                number: true,
+                range: [1, 10]
+            },
+
+
+
+        },
+        messages: {
+            title: "Palun sisesta pealkiri",
+            author: "Palun sisesta autor",
+            year: "Palun sisesta numbriline väärtus",
+            pages_total: {
+                required: "Palun sisesta lehekülgede number",
+                number: "Palun sisesta numbriline väärtus"
+            },
+            pages_finished: {
+                number: "Palun sisesta numbriline väärtus",
+                comparison: "Loetud arv ei saa olla suurem koguarvust"
+            },
+            rating: {
+                number: "Palun sisesta numbriline väärtus",
+                range: "Palun sisesta number vahemikus 1 kuni 10"
+            },
+
+        },
+        errorPlacement: function(error, element) {
+            // If input name is "title", then error is appended to a class named "error-title"
+            // This system applies to all input elements stated in rules
+            error.appendTo( $('.error-' + element.attr("name")));
+        }
+    });
 
 
 });
