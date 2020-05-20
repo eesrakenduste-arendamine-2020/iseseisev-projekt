0c342f156a19942d58f332765fb1aba942e0708f
Sandra Maidla <ss22nnuu@tlu.ee>
2020-05-20 14:20:02 +0300

    lisatud kommentaarid

--- .js
+++ .js
@@ -8,7 +8,6 @@ let baseURL = 'https://api.themoviedb.org/3/';
 const image_url = "https://image.tmdb.org/t/p/w185";
 let configData = null;
 let baseImageURL = null;
-
 let getConfig = function () {
     let url = "".concat(baseURL, 'configuration?api_key=', APIKEY); 
     fetch(url)
@@ -27,8 +26,6 @@ let getConfig = function () {
     });
 }
 
-
-//movie leht
 function movieSection(movies){
     return movies.map((movie) => {
         return `
@@ -64,6 +61,10 @@ let getPopularMovies = function(){
         const movies = data.results;
         const movieBlock = createMovies(movies);
         document.getElementById("movies").appendChild(movieBlock);
+        // console.log(data);
+        // console.log('popular movie data fetched');
+        // let string = JSON.stringify(data, null, 4);
+        // console.log(string);
     })
     .catch(function(err){
         alert(err);
@@ -71,52 +72,9 @@ let getPopularMovies = function(){
 }
 
 
+//var popular_movies;
 getPopularMovies();
 getConfig();
-
-//tvshow leht
-function showSection(tvshows){
-    return tvshows.map((show) => {
-        return `
-            <img src=${image_url + show.poster_path} data-movie-id=${show.id}/>
-        `;
-    })
-}
-
-function createShows(tvshows){
-    const element = document.createElement("div");
-    element.setAttribute("class", "show");
-
-    const template = `
-        <section class="section">
-            ${showSection(tvshows)};
-        </section>
-        <div class="content">
-            <p id="content-close">X</p>
-        </div>
-    `; 
-
-    element.innerHTML = template;
-    return element;
-}
-
-function getPopularTVshows(){
-    let url = "".concat(baseURL, 'tv/popular?api_key=', APIKEY, '&language=en-US&page=1');
-    fetch(url)
-    .then((result)=>{
-        return result.json();
-    })
-    .then((data)=>{
-        const tvshows = data.results;
-        const showBlock = createShows(tvshows);
-        document.getElementById("tv-shows").appendChild(showBlock);
-    })
-}
-
-getPopularTVshows();
-
-
-//klasside muutus
 $('.menu-item').click(function(){
     $('.menu-item').removeClass('active');
     $(this).addClass('active');
@@ -148,19 +106,3 @@ function selectContent(){
     }
 }
 
-// { "popularity": 517.523, 
-// "vote_count": 3420, 
-// "video": false, 
-// "poster_path": "/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg", 
-// "id": 419704, 
-// "adult": false, 
-// "backdrop_path": "/5BwqwxMEjeFtdknRV792Svo0K1v.jpg", 
-// "original_language": "en", 
-// "original_title": "Ad Astra", 
-// "genre_ids": [ 18, 878 ], 
-// "title": "Ad Astra", 
-// "vote_average": 6, 
-// "overview": "The near future, a time when both hope and hardships drive humanity to look to the stars and beyond.
-//  While a mysterious phenomenon menaces to destroy life on planet Earth, astronaut Roy McBride undertakes a mission across the immensity of space and its many perils to uncover
-//   the truth about a lost expedition that decades before boldly faced emptiness and silence in search of the unknown.", 
-//   "release_date": "2019-09-17" }
