5a91ada8e8f55b00315df2002c5aadf963f39eb2
Sandra Maidla <ss22nnuu@tlu.ee>
2020-05-20 13:47:36 +0300

    pildid popularmovies

--- tracker.js
+++ tracker.js
@@ -5,6 +5,7 @@ window.onload = function(){
 
 import{APIKEY} from "../lamp.js"; 
 let baseURL = 'https://api.themoviedb.org/3/';
+const image_url = "https://image.tmdb.org/t/p/w500";
 let configData = null;
 let baseImageURL = null;
 let getConfig = function () {
@@ -25,6 +26,31 @@ let getConfig = function () {
     });
 }
 
+function movieSection(movies){
+    return movies.map((movie) => {
+        return `
+            <img src=${image_url + movie.poster_path} data-movie-id=${movie.id}/>
+        `;
+    })
+}
+
+function createMovies(movies){
+    const element = document.createElement("div");
+    element.setAttribute("class", "movie");
+
+    const template = `
+        <section class="section">
+            ${movieSection(movies)};
+        </section>
+        <div class="content">
+            <p id="content-close">X</p>
+        </div>
+    `; 
+
+    element.innerHTML = template;
+    return element;
+}
+
 let getPopularMovies = function(){
     let url = "".concat(baseURL, 'movie/popular?api_key=', APIKEY, '&language=en-US');
     fetch(url)
@@ -32,8 +58,13 @@ let getPopularMovies = function(){
         return result.json();
     })
     .then((data)=>{
-        console.log(data);
-        console.log('popular movie data fetched');
+        const movies = data.results;
+        const movieBlock = createMovies(movies);
+        document.getElementById("movies").appendChild(movieBlock);
+        // console.log(data);
+        // console.log('popular movie data fetched');
+        // let string = JSON.stringify(data, null, 4);
+        // console.log(string);
     })
     .catch(function(err){
         alert(err);
@@ -74,3 +105,20 @@ function selectContent(){
         $('#search').hide();
     }
 }
+
+// { "popularity": 517.523, 
+// "vote_count": 3420, 
+// "video": false, 
+// "poster_path": "/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg", 
+// "id": 419704, 
+// "adult": false, 
+// "backdrop_path": "/5BwqwxMEjeFtdknRV792Svo0K1v.jpg", 
+// "original_language": "en", 
+// "original_title": "Ad Astra", 
+// "genre_ids": [ 18, 878 ], 
+// "title": "Ad Astra", 
+// "vote_average": 6, 
+// "overview": "The near future, a time when both hope and hardships drive humanity to look to the stars and beyond.
+//  While a mysterious phenomenon menaces to destroy life on planet Earth, astronaut Roy McBride undertakes a mission across the immensity of space and its many perils to uncover
+//   the truth about a lost expedition that decades before boldly faced emptiness and silence in search of the unknown.", 
+//   "release_date": "2019-09-17" }
