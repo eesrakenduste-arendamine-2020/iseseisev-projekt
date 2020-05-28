ae291f4f32c1c933b78bfb409686285adfdb8cff
Georg <georgha@tlu.ee>
2020-05-28 22:25:59 +0300

    Uuenduste ostmine ja peale panemine

--- clicker.js
+++ clicker.js
@@ -1,11 +1,52 @@
 let score_display = document.getElementById("score");
 let clickValue = 1;
+let idleValue = 0;
 let score = 0;
+let levels = [
+    ['passive1', 1],
+    ['passive2', 25],
+    ['passive3', 100],
+    ['passive4', 500],
+    ['passive5', 10000],
+    ['active1', 1],
+    ['active2', 25],
+    ['active3', 100],
+    ['active4', 500],
+    ['active5', 1000],
+];
+
+function updateScore(){
+    score_display.innerHTML = score;
+}
+function levelUp(entry){
+    for(let i=0; i < levels.length; i++){
+        if(entry === levels[i][0]){
+            if(i < 5){
+                idleValue += levels[i][1];
+            } else {
+                clickValue += levels[i][1];
+            }
+        }
+    }
+}
 
 $('#cookie').click(function(){
     score += clickValue;
-    score_display.innerHTML = score;
+    updateScore();
+
 });
+
+
+$('.btn').click(function(){
+    let price = parseInt(this.innerHTML)
+    if(score >= price){
+        this.innerHTML = Math.round(price+(price/2)).toString();
+        score -= price;
+        levelUp(this.id);
+        updateScore();
+    }
+});
+
 
 //Konami kood
 let allowedKeys = {
