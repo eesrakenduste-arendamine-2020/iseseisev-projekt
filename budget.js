a5e082016aa0760bb99b796f1a7e2b42dcafa3f3
psarri <psarri@users.noreply.github.com>
2020-06-02 14:21:42 +0300

    update2
    added edit and remove; local storage not working correctly

--- budget.js
+++ budget.js
@@ -13,7 +13,8 @@ class UI{
         this.itemList = [];
         this.itemID = 0;
 
-        //this.itemList = JSON.parse(window.localStorage.getItem('expenses')) || [];
+/*         this.itemList = JSON.parse(window.localStorage.getItem('expenses')) || [];
+        this.loadLocal(); */
 
     }
 
@@ -63,13 +64,13 @@ class UI{
         const div = document.createElement('div');
         div.classList.add('expense');
         div.innerHTML = `
-        <div class="expense-item">
+        <div id="expense-item" class="expense-item">
 
-         <h6 class="expense-title list-item">- ${expense.title}</h6>
-         <h5 class="expense-amount list-item">${expense.amount}</h5>
+         <p class="expense-title list-item">- ${expense.title}</p>
+         <p class="expense-amount list-item">${expense.amount}</p>
 
-          <img src="pencil.png" class="edit-icon" data-id="${expense.id}">
-          <img src="trash-can.png" class="delete-icon" data-id="${expense.id}">
+          <img src="pencil.png" id="edit-icon" class="edit-icon" data-id="${expense.id}">
+          <img src="trash-can.png" id="delete-icon" class="delete-icon" data-id="${expense.id}">
          </div>
         </div>
         `;
@@ -89,16 +90,53 @@ class UI{
         return total;
     }
 
+    removeExpense(element){
+        let id = parseInt(element.dataset.id);
+        let parent = element.parentElement.parentElement;
+
+        this.expenseList.removeChild(parent);
+
+        let tempList = this.itemList.filter(function(item){
+            return item.id !== id;
+        })
+
+        this.itemList = tempList;
+
+
+        this.showBalance();
+    }
+
     editExpense(element){
         let id = parseInt(element.dataset.id);
         let parent = element.parentElement.parentElement;
 
         this.expenseList.removeChild(parent);
+
+        let expense = this.itemList.filter(function(item){
+            return item.id === id;
+        })
+
+        this.expenseInput.value = expense[0].title;
+        this.amountInput.value = expense[0].amount;
+
+        let tempList = this.itemList.filter(function(item){
+            return item.id !== id;
+        })
+
+        this.itemList = tempList;
+        this.showBalance();
+
+
     }
 
-/*  saveLocal(){
+
+    saveLocal(){
         window.localStorage.setItem('expenses', JSON.stringify(this.itemList));
-    } */
+    }
+
+    loadLocal(){
+        window.localStorage.getItem('expenses', JSON.stringify(this.itemList));
+    }
 }
 
 function eventListener(){
@@ -119,18 +157,19 @@ function eventListener(){
     })
 
     expenseList.addEventListener('click', function(event){
-        if(event.target.parentElement.classList.contains('edit-icon')){
-            ui.editExpense(event.target.parentElement)
+        if(event.target.classList.contains('delete-icon')){
+            ui.removeExpense(event.target);
         }
     })
+
+    expenseList.addEventListener('click', function(event){
+        if(event.target.classList.contains('edit-icon')){
+            ui.editExpense(event.target);
+        }
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', function(){
     eventListener();
 })
-
-/* function changeColor(){
-   let back1 = document.getElementById("submit-background");
-   console.log(back1);
-   document.getElementsByClassName("grid-submit").style.backgroundColor = "#" + back1;
-} */
